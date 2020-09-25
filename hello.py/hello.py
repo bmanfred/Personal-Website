6ee23c7db42f05ddb54797e43468594fee5bf555
Brendan Manfred <bmanfred@nd.edu>
2020-09-25 12:46:46 -0400

    ssl not working

--- hello.py/hello.py
+++ hello.py/hello.py
@@ -20,8 +20,4 @@ def resume():
 
 
 if __name__ == '__main__':
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.verify_mode = ssl.CERT_REQUIRED
-    context.load_verify_locations('ca-crt.pem')
-    context.load_cert_chain('server.crt', 'server.key')
-    app.run(host = '0.0.0.0', port=8080, ssl_context=context)
+    app.run(host = '0.0.0.0', port=80)
